4a3bd43024d607a05cb6097312dc4b69421cb442
Adam Bouzourene <>
2018-07-02 08:49:49 -0400

    update remote repo with react functionality

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -5,7 +5,39 @@ class NavBar extends React.Component {
 
   render() {
     return (
-      <nav>IM THE NAV<ul></ul></nav>
+      <div className="navbar is-white">
+        <div className="navbar-brand">
+          <div className="navbar-burger burger" data-target="navMenuColorwhite-example">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="navMenuColorwhite-example" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item" href="#nav">
+              Home
+            </a>
+            <a className="navbar-item" href="#me-description">
+              Me
+            </a>
+            <a className="navbar-item" href="#skills">
+              Skills
+            </a>
+            <a className="navbar-item" href="#projects">
+              Projects
+            </a>
+            <a className="navbar-item" href="#companies">
+              Companies
+            </a>
+            <a className="navbar-item" href="#collab">
+              Collab
+            </a>
+            
+          </div>
+        </div>
+      </div>
     );
   }
 }
